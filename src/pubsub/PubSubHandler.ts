20c622a035cefb0527ea5d861a4613d7c20062cd
Karl Harder <karl.harder@roit.com.br>
2021-03-29 14:46:20 -0300

    chore: release 0.0.15

--- src/pubsub/PubSubHandler.ts
+++ src/pubsub/PubSubHandler.ts
@@ -24,7 +24,7 @@ export class PubSubHandler implements PublisherInterface {
         const message = Buffer.from(JSON.stringify(object))
         const messageId = await this.getInstance().topic(topicName).publish(message).catch(err => {
             console.error(err)
-            throw new Error('Error in PubSub publish method.')s
+            throw new Error('Error in PubSub publish method.')
         }).then(response => {
             return response
         })
